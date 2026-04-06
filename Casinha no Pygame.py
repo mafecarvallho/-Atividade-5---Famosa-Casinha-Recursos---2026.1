d827671b9ee65b70852285567023e10f886df6c3
mafecarvallho <mafecarvalho1710@gmail.com>
2026-04-06 12:47:41 -0300

    Add files via upload

--- Casinha no Pygame.py
+++ Casinha no Pygame.py
@@ -4,7 +4,7 @@ import sys
 
 init()
 
-passarinhos_png = image.load(' ')
+imagem = image.load('C:\Users\G2610993\Documents\-Atividade-5---Famosa-Casinha-Recursos---2026.1\pngtree-flock-of-birds-silhouettes-flying-birds-clipart-png-image_5371927.png')
 
 window = display.set_mode((1280 , 720))
 
@@ -17,8 +17,10 @@ while True:
             quit()
             sys.exit()
     
-    # desenhar a partir daqui
+    # desenhar a partir daqui - formas geometricas
     draw.rect(window , (100, 100, 100) , (300 , 300 , 200 ,250)), 
     draw.circle(window , (255, 242, 81), (150 , 100) , 50)
     draw.line( window , (255, 242, 81) , (200 , 200) , (100 , 100) , 5) 
+    # para desenhar a imagem
+    window.blit(imagem)
     display.update() 
